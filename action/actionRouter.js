8d3164a7ab4a9a6f5f4c415f0f2de66244d74a14
Mary Baker <meb601@gmail.com>
2020-10-02 12:06:03 -0400

    Populate action and project routers

--- action/actionRouter.js
+++ action/actionRouter.js
@@ -4,110 +4,65 @@ const Actions = require("../data/helpers/actionModel");
 
 const router = express.Router();
 
-// router.post("/", (req, res) => {
-//   Users.insert(req.body)
-//     .then((user) => {
-//       res.status(201).json(user);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error adding the user.",
-//       });
-//     });
-// });
+router.get("/", (req, res) => {
+  Actions.get(req.query)
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving actions.",
+      });
+    });
+});
 
-// router.post("/:id/posts", (req, res) => {
-//   const userInfo = { ...req.body, user_id: req.params.id };
+router.post("/", (req, res) => {
+  Actions.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding an action.",
+      });
+    });
+});
 
-//   Users.insert(userInfo)
-//     .then((user) => {
-//       res.status(210).json(user);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error getting the user for the post.",
-//       });
-//     });
-// });
+router.put("/:id", (req, res) => {
+  Actions.update(req.params.id, req.body)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "Action could not be found." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating action.",
+      });
+    });
+});
 
-// router.get("/", (req, res) => {
-//   Users.get(req.query)
-//     .then((users) => {
-//       res.status(200).json(users);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error retrieving the users.",
-//       });
-//     });
-// });
-
-// router.get("/:id", (req, res) => {
-//   Users.getById(req.params.id)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res.status(404).json({ message: "User not found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error retrieving the user.",
-//       });
-//     });
-// });
-
-// router.get("/:id/posts", (req, res) => {
-//   Users.getUserPosts(req.params.id)
-//     .then((posts) => {
-//       res.status(200).json(posts);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error getting the posts for the user.",
-//       });
-//     });
-// });
-
-// router.delete("/:id", (req, res) => {
-//   Users.remove(req.params.id)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json({ message: "The user has been deleted." });
-//       } else {
-//         res.status(404).json({ message: "The user could not be found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error removing the hub",
-//       });
-//     });
-// });
-
-// router.put("/:id", (req, res) => {
-//   Users.update(req.params.id, req.body)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res.status(404).json({ message: "The user could not be found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error updating the user.",
-//       });
-//     });
-// });
+router.delete("/:id", (req, res) => {
+  Actions.remove(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json({ message: "Action has been deleted." });
+      } else {
+        res.status(404).json({ message: "Action could not be found." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error deleting the action.",
+      });
+    });
+});
 
 // //custom middleware
 
--- project/projectRouter.js
+++ project/projectRouter.js
@@ -4,110 +4,65 @@ const Projects = require("../data/helpers/projectModel");
 
 const router = express.Router();
 
-// router.post("/", (req, res) => {
-//   Users.insert(req.body)
-//     .then((user) => {
-//       res.status(201).json(user);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error adding the user.",
-//       });
-//     });
-// });
+router.get("/", (req, res) => {
+  Projects.get(req.query)
+    .then((users) => {
+      res.status(200).json(users);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error retrieving projects.",
+      });
+    });
+});
 
-// router.post("/:id/posts", (req, res) => {
-//   const userInfo = { ...req.body, user_id: req.params.id };
+router.post("/", (req, res) => {
+  Projects.insert(req.body)
+    .then((user) => {
+      res.status(201).json(user);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error adding a project.",
+      });
+    });
+});
 
-//   Users.insert(userInfo)
-//     .then((user) => {
-//       res.status(210).json(user);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error getting the user for the post.",
-//       });
-//     });
-// });
+router.put("/:id", (req, res) => {
+  Projects.update(req.params.id, req.body)
+    .then((user) => {
+      if (user) {
+        res.status(200).json(user);
+      } else {
+        res.status(404).json({ message: "Project could not be found." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error updating project.",
+      });
+    });
+});
 
-// router.get("/", (req, res) => {
-//   Users.get(req.query)
-//     .then((users) => {
-//       res.status(200).json(users);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error retrieving the users.",
-//       });
-//     });
-// });
-
-// router.get("/:id", (req, res) => {
-//   Users.getById(req.params.id)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res.status(404).json({ message: "User not found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error retrieving the user.",
-//       });
-//     });
-// });
-
-// router.get("/:id/posts", (req, res) => {
-//   Users.getUserPosts(req.params.id)
-//     .then((posts) => {
-//       res.status(200).json(posts);
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error getting the posts for the user.",
-//       });
-//     });
-// });
-
-// router.delete("/:id", (req, res) => {
-//   Users.remove(req.params.id)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json({ message: "The user has been deleted." });
-//       } else {
-//         res.status(404).json({ message: "The user could not be found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error removing the hub",
-//       });
-//     });
-// });
-
-// router.put("/:id", (req, res) => {
-//   Users.update(req.params.id, req.body)
-//     .then((user) => {
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res.status(404).json({ message: "The user could not be found." });
-//       }
-//     })
-//     .catch((error) => {
-//       console.log(error);
-//       res.status(500).json({
-//         message: "Error updating the user.",
-//       });
-//     });
-// });
+router.delete("/:id", (req, res) => {
+  Projects.remove(req.params.id)
+    .then((user) => {
+      if (user) {
+        res.status(200).json({ message: "Project has been deleted." });
+      } else {
+        res.status(404).json({ message: "Project could not be found." });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "Error deleting the project.",
+      });
+    });
+});
 
 // //custom middleware
 
